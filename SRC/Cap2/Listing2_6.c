034c5f608ccea8b46a68b3bf2d83a9af42ca581b
carter <jonathanalcaraz.e@fpuna.edu.py>
2023-09-18 12:30:04 -0400

    Main 2_6

--- SRC/Cap2/Listing2_6.c
+++ SRC/Cap2/Listing2_6.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 
 char* read_from_file (const char* filename, size_t length)
 {
@@ -32,4 +33,20 @@ char* read_from_file (const char* filename, size_t length)
   /* Everything's fine.  Close the file and return the buffer.  */
   close (fd);
   return buffer;
+}
+
+int main() {
+  const char* filename = "HolaMundo.txt"; // Nombre del archivo a leer
+  size_t length = 10; // Longitud deseada
+
+  char* file_contents = read_from_file(filename, length);
+
+  if (file_contents != NULL) {
+    printf("Contenido del archivo:\n%s\n", file_contents);
+    free(file_contents);
+  } else {
+    printf("Error al leer el archivo.\n");
+  }
+
+  return 0;
 }
